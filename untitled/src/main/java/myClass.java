88fa8453fd6487ff6b99ef075d7cdd10524b3fb5
Roger.Farber <rogerfarber@gmail.com>
2021-06-20 13:44:57 +0300

    try this

--- untitled/src/main/java/myClass.java
+++ untitled/src/main/java/myClass.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class myClass {
     public String printMe(){
         return "Hello worls";
     }
     public static void main(String[] args) {
         myClass obj = new myClass();
+
         System.out.println(obj.printMe());
     }
 }
