ab803382ef01908ddbfe68138a4fe9cfc55fee69
Roger.Farber <rogerfarber@gmail.com>
2021-06-20 13:33:23 +0300

    wrote code

---
+++ untitled/src/main/java/myClass.java
@@ -0,0 +1,9 @@
+public class myClass {
+    public String printMe(){
+        return "Hello worls";
+    }
+    public static void main(String[] args) {
+        myClass obj = new myClass();
+        System.out.println(obj.printMe());
+    }
+}
