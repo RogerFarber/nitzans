008ec6a34bbf49b6ea8ac1e3e63ae87bec00c32c
Roger.Farber <rogerfarber@gmail.com>
2021-06-20 12:07:52 +0300

    another test

--- src/test/java/my_PrintTest.java
+++ src/test/java/my_PrintTest.java
@@ -3,9 +3,5 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class my_PrintTest {
-
-    @Test
-    public void testPrintme() {
-        assertEquals("Hello Word!","Hello Word!");
-    }
+    
 }
