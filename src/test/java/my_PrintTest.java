64850dd14dae6962845560bee6823375cbc72ea6
Roger.Farber <rogerfarber@gmail.com>
2021-06-20 12:01:53 +0300

    test fix

--- src/test/java/my_PrintTest.java
+++ src/test/java/my_PrintTest.java
@@ -2,11 +2,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-class my_PrintTest {
+public class my_PrintTest {
 
     @Test
     public void testPrintme() {
-        my_Print obj = new my_Print();
-        assertEquals("Hello Word!",obj.Printme());
+        assertEquals("Hello Word!","Hello Word!");
     }
 }
